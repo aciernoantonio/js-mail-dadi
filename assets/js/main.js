d562e49f728b1897dad3f8cbd7ee002c82336ae8
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-29 15:46:56 +0200

    add dice script section

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@
 */
 
 const mail_list = ["baby.yoda@gmail.com", "pincopallino@gmail.com", "fraccopacco@gmail.com"];
-console.log(mail_list);
 
 //chiedere all'utente la sua mail
 
+alert("Benvenuto a Js Dice!");
 let user_mail = prompt("Inserisci la tua mail");
-console.log(user_mail);
+
 
 //controlla che sia nella lista
 if (mail_list.includes(user_mail)) {
@@ -16,7 +16,7 @@ if (mail_list.includes(user_mail)) {
     //stampa esito del controllo
     alert("La mail è presente nell'elenco");
 } else {
-    
+
     //stampa esito del controllo
     alert("La mail non è presente nell'elenco");
 }
@@ -29,7 +29,20 @@ if (mail_list.includes(user_mail)) {
 */
 
 //genera un numero random per giocatore
+let user_num = Math.floor(Math.random() * 6) + 1;
+
 
 //genera un numero random per computer
+let cpu_num = Math.floor(Math.random() * 6) + 1;
+
 
 //stabilire vincitore con valore più alto
+if (user_num > cpu_num) {
+    alert("L'utente ha vinto!");
+
+} else if (user_num == cpu_num) {
+    alert("Pareggio!");
+
+} else {
+    alert("Il computer ha vinto!");
+}
