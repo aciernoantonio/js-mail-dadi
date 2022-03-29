f3eb92ce8fe7d10e4f5ff35159ad86d8666cb7e6
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-29 15:10:35 +0200

    add questions for problem resolution

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/* 
+    parte della mail
+*/
+
+//chiedere all'utente la sua mail
+
+//controlla che sia nella lista
+
+//stampa esito del controllo
+
+
+/* 
+    gioco dei dadi
+*/
+
+//genera un numero random per giocatore
+
+//genera un numero random per computer
+
+//stabilire vincitore con valore più alto
